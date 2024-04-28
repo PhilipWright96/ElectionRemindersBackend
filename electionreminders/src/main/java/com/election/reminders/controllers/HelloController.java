c9f1ab8016b8365e1cbca9f0de2c51e5b549e77b
Philip Wright <wrightphilip4@gmail.com>
2024-04-28 19:04:47 +0200

    Add basic JSON response to controller

--- electionreminders/src/main/java/com/election/reminders/controllers/HelloController.java
+++ electionreminders/src/main/java/com/election/reminders/controllers/HelloController.java
@@ -4,12 +4,26 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.jr.ob.JSON;
+
 @RestController
 public class HelloController {
     @CrossOrigin(origins = "http://localhost:8100")
     @GetMapping("/test")
     public String index() {
-        return "Hello World!";
+        String json = null;
+        try {
+            json = JSON.std.with(JSON.Feature.PRETTY_PRINT_OUTPUT)
+                    .composeString()
+                    .startObject()
+                    .put("test", "testString")
+                    .put("test2", 1)
+                    .end()
+                    .finish();
+        } catch (Exception e) {
+            System.out.println("Exception in GET request " + e.getMessage());
+        }
+        return json;
     }
 
 }
