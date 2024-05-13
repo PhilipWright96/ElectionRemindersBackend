592262a4d73285a513b9eebcf077bfbe26ee6bb1
Philip Wright <wrightphilip4@gmail.com>
2024-05-13 20:48:14 +0200

    Add get request which receives param to controller

--- electionreminders/src/main/java/com/election/reminders/controllers/ElectionController.java
+++ electionreminders/src/main/java/com/election/reminders/controllers/ElectionController.java
@@ -1,5 +1,27 @@
 package com.election.reminders.controllers;
 
-public class ElectionController {
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.jr.ob.JSON;
+
+@RestController
+public class ElectionController {
+    @CrossOrigin(origins = Constants.FRONT_END_URL)
+    @GetMapping("/electionsForCountry")
+    public String getElectionsForCountry() {
+        String json = null;
+        try {
+            json = JSON.std.with(JSON.Feature.PRETTY_PRINT_OUTPUT)
+                    .composeString()
+                    .startObject()
+                    .put("electionName", "a")
+                    .end()
+                    .finish();
+        } catch (Exception e) {
+            System.out.println("Exception in GET request " + e.getMessage());
+        }
+        return json;
+    }
 }
