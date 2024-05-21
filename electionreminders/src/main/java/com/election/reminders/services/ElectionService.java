749e4cdc7f5fda631bb2036f533cb094779bdde5
Philip Wright <wrightphilip4@gmail.com>
2024-05-21 14:35:43 +0200

    Extract out logic to a repository class

--- electionreminders/src/main/java/com/election/reminders/services/ElectionService.java
+++ electionreminders/src/main/java/com/election/reminders/services/ElectionService.java
@@ -1,18 +1,13 @@
 package com.election.reminders.services;
 
 import com.election.reminders.dtos.responses.TestResponse;
+import com.election.reminders.repositories.ElectionRepository;
 
 import java.util.List;
 
 public class ElectionService {
     public List<TestResponse> getElectionsForCountry(String countryName) {
-        TestResponse testResponse1 = new TestResponse();
-        testResponse1.electionName = "Germany";
-
-        TestResponse testResponse2 = new TestResponse();
-        testResponse2.electionName = "France";
-
-        final List<TestResponse> responses = List.of(testResponse1, testResponse2);
-        return responses;
+        // Should election repository be a bean here?
+        return new ElectionRepository().getElectionsForCountry(countryName);
     }
 }
