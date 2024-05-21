200da88a0381456904b685d85f01b4271be98957
Philip Wright <wrightphilip4@gmail.com>
2024-05-21 14:18:33 +0200

    Extract out JSON conversion logic to new class

--- electionreminders/src/main/java/com/election/reminders/controllers/ElectionController.java
+++ electionreminders/src/main/java/com/election/reminders/controllers/ElectionController.java
@@ -9,25 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.election.reminders.dtos.responses.TestResponse;
 import com.election.reminders.services.ElectionService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.election.reminders.utils.JavaToJSONConverter;
 
 @RestController
 public class ElectionController {
     @CrossOrigin(origins = Constants.FRONT_END_URL)
     @GetMapping("/electionsForCountry")
     public String getElectionsForCountry(@RequestParam String countryName) {
-        // Should Election Service be a bean?
+        // Should Election Service and Java Converter be a bean?
         final List<TestResponse> electionsForCountry = new ElectionService().getElectionsForCountry(countryName);
-        final ObjectMapper objectMapper = new ObjectMapper();
-
-        String json = null;
-
-        try {
-            json = objectMapper.writeValueAsString(electionsForCountry);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return json;
+        return new JavaToJSONConverter().convertJavaToJSON(electionsForCountry);
     }
 }
---
+++ electionreminders/src/main/java/com/election/reminders/utils/JavaToJSONConverter.java
@@ -0,0 +1,21 @@
+package com.election.reminders.utils;
+
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JavaToJSONConverter {
+    public <T> String convertJavaToJSON(List<T> objectsToConvertToJSON) {
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        String json = null;
+
+        try {
+            json = objectMapper.writeValueAsString(objectsToConvertToJSON);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+}
