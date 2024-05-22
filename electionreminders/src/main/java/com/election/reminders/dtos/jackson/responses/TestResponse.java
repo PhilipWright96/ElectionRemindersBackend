794ac02eeb935b01d7f6ba89859f6ba46cd1c284
Philip Wright <wrightphilip4@gmail.com>
2024-05-22 16:07:55 +0200

    Use builder in TestResponse constructor

--- electionreminders/src/main/java/com/election/reminders/dtos/jackson/responses/TestResponse.java
+++ electionreminders/src/main/java/com/election/reminders/dtos/jackson/responses/TestResponse.java
@@ -1,8 +1,25 @@
 package com.election.reminders.dtos.jackson.responses;
 
+import com.election.reminders.builders.TestResponseBuilder;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class TestResponse {
+    public TestResponse(TestResponseBuilder builder) {
+        this.electionName = builder.getElectionName();
+        this.electionDate = builder.getElectionDate();
+        this.electionSummary = builder.getElectionSummary();
+        this.isRepeating = builder.getIsRepeating();
+        this.repeatingEvery = builder.getRepeatingEvery();
+    }
+
     @JsonProperty("electionName")
     public String electionName;
+    @JsonProperty("electionDate")
+    public String electionDate;
+    @JsonProperty("electionSummary")
+    public String electionSummary;
+    @JsonProperty("isRepeating")
+    public Boolean isRepeating;
+    @JsonProperty("repeatingEvery")
+    public String repeatingEvery;
 }
