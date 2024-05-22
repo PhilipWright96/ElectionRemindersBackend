c9c4407cbc30df804bde616ad887a4f725d0080f
Philip Wright <wrightphilip4@gmail.com>
2024-05-22 16:13:23 +0200

    Extract out dummy data creation to private method

--- electionreminders/src/main/java/com/election/reminders/repositories/ElectionRepository.java
+++ electionreminders/src/main/java/com/election/reminders/repositories/ElectionRepository.java
@@ -10,11 +10,15 @@ import com.election.reminders.dtos.jackson.responses.TestResponse;
 @Repository
 public class ElectionRepository {
     public List<TestResponse> getElectionsForCountry(String countryName) {
+        return constructDummyTestResponses();
+    }
+
+    private List<TestResponse> constructDummyTestResponses() {
         TestResponse testResponse1 = new TestResponseBuilder().electionName("A").electionDate("Date for A")
-                .electionSummary("Summary for A").build();
+                .electionSummary("Summary for A").isRepeating(false).build();
 
         TestResponse testResponse2 = new TestResponseBuilder().electionName("B").electionDate("Date for B")
-                .electionSummary("Summary for B").build();
+                .electionSummary("Summary for B").isRepeating(true).repeatingEvery("3 years").build();
 
         final List<TestResponse> responses = List.of(testResponse1, testResponse2);
         return responses;
