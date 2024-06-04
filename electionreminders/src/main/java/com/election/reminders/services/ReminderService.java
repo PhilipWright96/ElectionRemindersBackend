cc68f41932c5eef2118a4656acf9e8a83f72428a
Philip Wright <wrightphilip4@gmail.com>
2024-06-04 21:06:46 +0200

    Add reminder service

---
+++ electionreminders/src/main/java/com/election/reminders/services/ReminderService.java
@@ -0,0 +1,23 @@
+package com.election.reminders.services;
+
+import com.election.reminders.dtos.jackson.responses.ReminderInformation;
+import com.election.reminders.repositories.ReminderRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReminderService {
+    private final ReminderRepository reminderRepository;
+
+    @Autowired
+    public ReminderService(ReminderRepository reminderRepository) {
+        this.reminderRepository = reminderRepository;
+    }
+
+    public List<ReminderInformation> getRemindersForUser(String userName) {
+        return reminderRepository.getRemindersForUser(userName);
+    }
+}
