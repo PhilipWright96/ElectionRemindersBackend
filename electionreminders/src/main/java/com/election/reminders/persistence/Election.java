f899c0e9d8d50350182e0255308829935a3ddc99
Philip Wright <wrightphilip4@gmail.com>
2025-03-09 19:16:22 +0100

    Add election entity object

---
+++ electionreminders/src/main/java/com/election/reminders/persistence/Election.java
@@ -0,0 +1,39 @@
+package com.election.reminders.persistence;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "elections")
+@Getter
+@Setter
+public class Election {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID electionId;
+
+    @Column(nullable = false)
+    private String electionName;
+
+    @Column(nullable = false)
+    private String countryName;
+
+    @Column(nullable = false)
+    private LocalDateTime electionDate;
+
+    private String electionSummary;
+
+    private Boolean isRepeating;
+
+    private String repeatingEvery;
+
+}
