f4256d8bb9acc9b2ff1133e37115e181f68a5c94
Philip Wright <wrightphilip4@gmail.com>
2025-03-13 20:13:47 +0100

    Remove outdated reminder logic

--- electionreminders/src/main/java/com/election/reminders/utils/TestDataBuilder.java
+++ electionreminders/src/main/java/com/election/reminders/utils/TestDataBuilder.java
@@ -2,12 +2,9 @@ package com.election.reminders.utils;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 import com.election.reminders.builders.ElectionInformationBuilder;
-import com.election.reminders.builders.ReminderInformationBuilder;
 import com.election.reminders.dtos.jackson.responses.ElectionInformationDto;
-import com.election.reminders.dtos.jackson.responses.ReminderInformation;
 
 public class TestDataBuilder {
         public static List<ElectionInformationDto> constructDummyElectionInformations() {
@@ -24,30 +21,5 @@ public class TestDataBuilder {
                                 .electionSummary("Summary for C").isRepeating(true).repeatingEvery("3 years").build();
 
                 return List.of(electionInformation1, electionInformation2, electionInformation3);
-        }
-
-        public static List<ReminderInformation> constructDummyReminderInformations() {
-                ReminderInformation reminderInformation1 = new ReminderInformationBuilder()
-                                .reminderName("Reminder For A")
-                                .electionId(UUID.randomUUID())
-                                .reminderDate(LocalDateTime.of(2024, 1, 1, 1, 0))
-                                .createdOn(LocalDateTime.of(2024, 1, 2, 1, 0))
-                                .reminderDetails("Details for reminder A").build();
-
-                ReminderInformation reminderInformation2 = new ReminderInformationBuilder()
-                                .reminderName("Reminder For B")
-                                .electionId(UUID.randomUUID())
-                                .reminderDate(LocalDateTime.of(2024, 2, 1, 1, 0))
-                                .createdOn(LocalDateTime.of(2024, 2, 2, 1, 0))
-                                .reminderDetails("Details for reminder B").build();
-
-                ReminderInformation reminderInformation3 = new ReminderInformationBuilder()
-                                .reminderName("Reminder For C")
-                                .electionId(UUID.randomUUID())
-                                .reminderDate(LocalDateTime.of(2024, 3, 1, 1, 0))
-                                .createdOn(LocalDateTime.of(2024, 3, 2, 1, 0))
-                                .reminderDetails("Details for reminder c").build();
-
-                return List.of(reminderInformation1, reminderInformation2, reminderInformation3);
         }
 }
