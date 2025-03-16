11fe0bc7838357bebc729c5eacc43285dec642a8
Philip Wright <wrightphilip4@gmail.com>
2025-03-16 14:45:01 +0100

    Remove unused import

--- electionreminders/src/main/java/com/election/reminders/services/ElectionService.java
+++ electionreminders/src/main/java/com/election/reminders/services/ElectionService.java
@@ -6,7 +6,6 @@ import com.election.reminders.repositories.ElectionRepository;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
