59dee9679fe312b5fd17832e530eebf3f2d35b2e
Philip Wright <wrightphilip4@gmail.com>
2025-07-12 19:32:32 +0200

    Update test data builder

--- electionreminders/src/main/java/com/election/reminders/utils/TestDataBuilder.java
+++ electionreminders/src/main/java/com/election/reminders/utils/TestDataBuilder.java
@@ -8,15 +8,15 @@ import com.election.reminders.dtos.jackson.responses.ElectionInformationDto;
 
 public class TestDataBuilder {
         public static List<ElectionInformationDto> constructDummyElectionInformations() {
-                ElectionInformationDto electionInformation1 = new ElectionInformationBuilder().electionName("A")
+                ElectionInformationDto electionInformation1 = new ElectionInformationBuilder().electionName("Backend A")
                                 .electionDate(LocalDateTime.of(2025, 4, 1, 1, 0))
                                 .electionSummary("Summary for A").isRepeating(false).build();
 
-                ElectionInformationDto electionInformation2 = new ElectionInformationBuilder().electionName("B")
+                ElectionInformationDto electionInformation2 = new ElectionInformationBuilder().electionName("Backend B")
                                 .electionDate(LocalDateTime.of(2025, 5, 1, 1, 0))
                                 .electionSummary("Summary for B").isRepeating(true).repeatingEvery("3 years").build();
 
-                ElectionInformationDto electionInformation3 = new ElectionInformationBuilder().electionName("C")
+                ElectionInformationDto electionInformation3 = new ElectionInformationBuilder().electionName("Backend C")
                                 .electionDate(LocalDateTime.of(2026, 6, 1, 1, 0))
                                 .electionSummary("Summary for C").isRepeating(true).repeatingEvery("3 years").build();
 
