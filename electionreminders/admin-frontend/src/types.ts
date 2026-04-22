b7ac389e7e125a03b01989a3261f2c0c6660648b
Philip Wright <wrightphilip4@gmail.com>
2026-04-22 20:02:46 +0200

    Update types

--- electionreminders/admin-frontend/src/types.ts
+++ electionreminders/admin-frontend/src/types.ts
@@ -3,17 +3,26 @@ export type ElectionSuggestionFromBackend = {
     electionName: string;
     electionType: string;
     electionArea: string;
+    countryName: string;
+    regionName: string;
+    cityName: string;
+    organizationName: string;
     electionPollsOpenDateTime: string;   // usually ISO string from backend
     electionPollsCloseDateTime: string;  // same here
     electionDetails: string;
+    electionSummary: string;
 };
 
 export type ElectionSuggestion = {
     id: number;
     electionName: string;
     electionType: string;
-    electionArea: string;
+    countryName: string;
+    regionName: string;
+    cityName: string;
+    organizationName: string;
     electionPollsOpenDateTime: string;
     electionPollsCloseDateTime: string;
     electionDetails: string;
+    electionSummary: string;
 };
