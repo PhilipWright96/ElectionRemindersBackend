46da5241529aaa4577c5880da0beeb86e21ad238
Philip Wright <wrightphilip4@gmail.com>
2026-08-03 22:47:45 +0200

    Add new admin dashboard

--- electionreminders/admin-frontend/src/App.tsx
+++ electionreminders/admin-frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Dashboard from './Dashboard'
+import ElectionEntityDashboard from './ElectionEntityDashboard';
 
 function App() {
   return (
     <div>
       <h1>Admin Front End for Election Reminders</h1>
       <Dashboard />
+      <h1> Dashboard for Election Entities</h1>
+      <ElectionEntityDashboard />
     </div>
   );
 }
